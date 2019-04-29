eb1428149c4be7c878d7548c12a96509c606d669
michaelouan <49525723+michaelouan@users.noreply.github.com>
2019-04-29 15:07:45 +0300

    Create tar3.cpp

---
+++ tar3.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int arr[30];
+//	int number = 0;
+//	int count = 0;
+//
+//	for (int i = 0; i < 30; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	while (number < 10)
+//	{
+//		for (int i = 0; i < 30; i++)
+//		{
+//			if (arr[i] == number)
+//			{
+//				count++;
+//			}
+//		}
+//
+//		cout << "The number " << number << " appear " << count << " times" << endl;
+//		number++;
+//		count = 0;
+//	}
+//}
