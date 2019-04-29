7a752dab39d7f7ef7d6c9882a40e8d53e648b437
michaelouan <49525723+michaelouan@users.noreply.github.com>
2019-04-29 15:07:18 +0300

    Create tar2.cpp

---
+++ tar2.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int arr[30];
+//	int number;
+//	int count = 0;
+//
+//	for (int i = 0; i < 30; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	cout << "enter a number" << endl;
+//	cin >> number;
+//
+//	for (int i = 0; i < 30; i++)
+//	{
+//		if (arr[i] == number)
+//		{
+//			count++;
+//			if (arr[i + 1] != number)
+//			{
+//				break;
+//			}
+//		}
+//	}
+//
+//	cout << "The number " << number << " appear " << count << " times" << endl;
+//}
