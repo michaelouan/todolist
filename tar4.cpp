6ebcb2ccc5fa264293d7cab1f970f3839e8a839e
michaelouan <49525723+michaelouan@users.noreply.github.com>
2019-04-29 15:08:04 +0300

    Create tar4.cpp

---
+++ tar4.cpp
@@ -0,0 +1,39 @@
+//#include<iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int arr[10];
+//	int arr1[3];
+//	bool is = false;
+//
+//	cout << "Enter the first array" << endl;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	cout << "Enter the second array" << endl;
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cin >> arr1[i];
+//	}
+//
+//	for (int i = 0; i < 3; i++)
+//	{	
+//		is = false;
+//		for (int j = 0; j < 10; j++)
+//		{
+//			if (arr[j] == arr1[i])
+//			{
+//				is = true;
+//				break;
+//			}
+//		}
+//
+//		if (!is) {
+//			break;
+//		}
+//	}
+//	cout << is;
+//}
