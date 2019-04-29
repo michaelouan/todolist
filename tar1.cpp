da563d4bb45fda76de32a764feabcc349d9646b1
michaelouan <49525723+michaelouan@users.noreply.github.com>
2019-04-29 15:07:00 +0300

    Create tar1.cpp

---
+++ tar1.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//void main()
+//{
+//	int a;
+//	int help;
+//	int helpp;
+//	cout << "Enter a number " << endl;
+//	cin >> a;
+//
+//	char arr[26];
+//
+//	for (int i = 0; i < 26; i++)
+//	{
+//		arr[i] = 65 + i;
+//	}
+//
+//	help = a;
+//	while (help > 0)
+//	{
+//		helpp = (help - 1) % 26;
+//		cout << arr[helpp];
+//		help = (help - helpp) / 26;
+//	}
+//}
