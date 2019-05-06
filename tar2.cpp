b7232574be88fa117a19848c40a2781e88bafe78
michaelouan <49525723+michaelouan@users.noreply.github.com>
2019-05-06 13:10:06 +0300

    Update tar2.cpp

--- tar2.cpp
+++ tar2.cpp
@@ -1,31 +1,66 @@
-//#include<iostream>
-//using namespace std;
-//
-//void main()
-//{
-//	int arr[30];
-//	int number;
-//	int count = 0;
-//
-//	for (int i = 0; i < 30; i++)
-//	{
-//		cin >> arr[i];
-//	}
-//
-//	cout << "enter a number" << endl;
-//	cin >> number;
-//
-//	for (int i = 0; i < 30; i++)
-//	{
-//		if (arr[i] == number)
-//		{
-//			count++;
-//			if (arr[i + 1] != number)
-//			{
-//				break;
-//			}
-//		}
-//	}
-//
-//	cout << "The number " << number << " appear " << count << " times" << endl;
-//}
+#include<iostream>
+using namespace std;
+
+int countOccurrences(int arr[], int n, int x);
+int binarySearch(int arr[], int l, int r, int x);
+
+int main()
+{
+	int arr[10];
+	int x;
+
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+	}
+	cout << "Enter a number" << endl;
+	cin >> x;
+
+	cout <<"The number "<<x<<" appear "<< countOccurrences(arr, 10, x) <<" times"<<endl;
+}
+
+int binarySearch(int arr[], int l, int r, int numToSearch)
+{
+	int middle = l + (r - l) / 2;
+
+	if (r < l)
+	{
+		return -1;
+	}
+	else if (arr[middle] == numToSearch)
+	{
+		return middle;
+	}
+	else if (arr[middle] > numToSearch)
+	{
+		return binarySearch(arr, l, middle - 1, numToSearch);
+	}
+
+	return binarySearch(arr, middle + 1, r, numToSearch);
+}
+
+int countOccurrences(int arr[], int ArrayLength, int numToSearch)
+{
+	int index = binarySearch(arr, 0, ArrayLength - 1, numToSearch);
+	int count = 1;
+	int left = index - 1;
+	int right = index + 1;
+
+	if (index == -1)
+	{
+		return 0;
+	}
+		
+	while (left >= 0 && arr[left] == numToSearch)
+	{
+		count++;
+		left--;
+	}
+
+	while (right < ArrayLength && arr[right] == numToSearch)
+	{
+		count++;
+		right++;
+	}
+
+	return count;
+}
